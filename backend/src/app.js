7ca7076339d60bea7957afc2aa5be7c40c938cf0
suspicious115478 <sus24picious45@gmail.com>
2025-11-24 10:19:45 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const agentRoutes = require("./routes/agentRoutes");
 const callRoutes = require("./routes/callRoutes");
 const webrtcRoutes = require("./routes/webrtcRoutes");
-
+const logRoutes = require("./routes/logRoutes"); 
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -12,7 +12,9 @@ app.use(cors());
 app.use("/agent", agentRoutes);
 app.use("/call", callRoutes);
 app.use("/webrtc", webrtcRoutes);
+app.use("/api/logs", logRoutes); 
 
 module.exports = app;
+
 
 
