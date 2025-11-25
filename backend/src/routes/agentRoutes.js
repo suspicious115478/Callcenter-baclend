0f189380f838f9a05a78e6d5197e505bf41c3001
suspicious115478 <sus24picious45@gmail.com>
2025-11-25 17:59:56 +0530

    Delete backend directory

--- backend/src/routes/agentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getStatus, setStatus } = require("../controllers/agentController");
-const router = express.Router();
-
-router.get("/status", getStatus);
-router.post("/status", setStatus);
-
-module.exports = router;
