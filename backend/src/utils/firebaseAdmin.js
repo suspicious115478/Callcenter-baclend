8883494b4aacff9faa0aa4a6cb92cf407bd57657
suspicious115478 <sus24picious45@gmail.com>
2025-11-29 13:40:07 +0530

    Update firebaseAdmin.js

--- backend/src/utils/firebaseAdmin.js
+++ backend/src/utils/firebaseAdmin.js
@@ -3,7 +3,7 @@ const admin = require('firebase-admin');
 
 // ⚠️ IMPORTANT: Replace this with the path to your Firebase service account key file
 // You can download this file from your Firebase Project Settings -> Service Accounts
-const serviceAccount = require('./path-to-your-serviceAccountKey.json'); 
+const serviceAccount = require('./FIREBASE_SERVICE_ACCOUNT_KEY'); 
 
 // ⚠️ IMPORTANT: Replace this with your Realtime Database URL
 const databaseURL = 'https://project-8812136035477954307-default-rtdb.firebaseio.com/';
