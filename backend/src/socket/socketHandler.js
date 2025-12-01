cd6fbcba416897cf042192464c1e202ddc2695f5
suspicious115478 <sus24picious45@gmail.com>
2025-12-01 16:04:16 +0530

    Update socketHandler.js

--- backend/src/socket/socketHandler.js
+++ backend/src/socket/socketHandler.js
@@ -28,7 +28,7 @@ exports.setupSocket = (server) => {
       }
 
       // 2. Proceed only if Online
-      const testNumber = "91987654444"; // Using 10-digit format to match DB
+      const testNumber = "919812300001"; // Using 10-digit format to match DB
       
       const callData = await callController.checkSubscriptionStatus(testNumber);
       
@@ -47,6 +47,7 @@ exports.setupSocket = (server) => {
 };
 
 exports.io = () => ioInstance;
+
 
 
 
